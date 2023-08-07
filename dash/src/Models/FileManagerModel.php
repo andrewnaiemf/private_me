290dafb2532bbcfc3d952a27e41fdd39d602ee78
andrewnaiemf <andrewnaiemf@gmail.com>
2023-08-07 15:43:46 +0300

    test access first page f pdf file

--- dash/src/Models/FileManagerModel.php
+++ dash/src/Models/FileManagerModel.php
@@ -3,6 +3,9 @@ namespace Dash\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Imagick;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class FileManagerModel extends Model {
 	use SoftDeletes;
@@ -22,6 +25,32 @@ class FileManagerModel extends Model {
 		'mimtype',
         'directory_id'
 	];
+    protected $appends = ['first_page_image'];
+
+    public function getFirstPageImageAttribute()
+    {
+        if ($this->file_id == 3 && in_array($this->ext, ['pdf'])) {
+
+            if (Storage::exists($this->full_path)) {
+                $imagick = new Imagick();
+                $imagick->readImage($$this->full_path . '[0]'); // Read only the first page
+                $imgSavePath = $this->path . Str::beforeLast($this->name, '.') . '.png';
+                if (!Storage::exists($imgSavePath)) {
+                    $imgSavePath = storage_path($imgSavePath);
+                    $imagick->setImageFormat('png');
+                    $imagick->writeImage($imgSavePath);
+                    return Storage::url($imgSavePath);
+                }else{
+                    return $imgSavePath;
+                }
+
+            } else {
+                // Handle case where the PDF file doesn't exist
+                return null;
+            }
+        }
+    }
+
 
 	public function user() {
 		return \DB::table('users')->where('id', $this->user_id)->first();
